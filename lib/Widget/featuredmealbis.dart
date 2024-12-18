0acb21155eab442edfb84022a1b884bcf18a9e86
Omecraft Pro <pierre@bleuzen.net>
2024-12-18 22:43:05 +0100

    Improvement of featured meal

--- lib/Widget/featuredmealbis.dart
+++ lib/Widget/featuredmealbis.dart
@@ -1,46 +1,81 @@
 import 'package:autochiefv2/Widget/slidable.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:iconly/iconly.dart';
 
 class Featuredmealbis extends StatelessWidget {
+  final String data;
   final String image;
-  const Featuredmealbis({super.key,required this.image});
+  final String difficulty;
+  const Featuredmealbis({super.key,required this.image,required this.data,required this.difficulty});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      width: 200,
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16), // Bordures arrondies
-            child: Image.network(
-              image, // Remplace par ton URL
-              width: 200, // Largeur personnalisée
-              height: 300, // Hauteur personnalisée
-              fit: BoxFit.cover, // L'image sera coupée pour remplir les dimensions
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 300,
+        width: 200,
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16), // Bordures arrondies
+              child: Image.network(
+                image, // Remplace par ton URL
+                width: 200, // Largeur personnalisée
+                height: 300, // Hauteur personnalisée
+                fit: BoxFit.cover, // L'image sera coupée pour remplir les dimensions
+              ),
             ),
-          ),
-          Positioned(top: 10, right: 10, 
-          child: Container(decoration:BoxDecoration(
-            border: const Border(
-              top: BorderSide(color: Colors.white, width: 2),
-              bottom: BorderSide(color: Colors.white, width: 2),
-              left: BorderSide(color: Colors.white, width: 2),
-              right: BorderSide(color: Colors.white, width: 2),
-            ),
-            borderRadius: BorderRadius.circular(90),),
-          
-          child: const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Icon(IconlyLight.heart,color: Colors.white,),
-          ))),
-          
-          const Positioned(child: Slidable(),bottom: 0,)
-          ]
+            Positioned(top: 10, right: 10, 
+            child: Container(decoration:BoxDecoration(
+              border: const Border(
+                top: BorderSide(color: Color(0xFFffb17a), width: 2),
+                bottom: BorderSide(color: Color(0xFFffb17a), width: 2),
+                left: BorderSide(color: Color(0xFFffb17a), width: 2),
+                right: BorderSide(color: Color(0xFFffb17a), width: 2),
+              ),
+              borderRadius: BorderRadius.circular(90),),
+            
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(IconlyLight.heart,color: Color(0xFFffb17a),),
+            ))),
+            Positioned(bottom: 100,child: Padding(
+              padding: const EdgeInsets.only(left:10.0),
+              child: Text(difficulty,style: GoogleFonts.nunito(
+                color: Color(0xFFffb17a),
+                fontSize: 15,
+                shadows: [
+                    const Shadow(
+                      blurRadius: 1.0,
+                      color: Colors.black,
+                      offset: Offset(0.0, 0.0),
+                    ),
+                  ]
+              ),),
+            ),),
+            Positioned(bottom: 65,child: Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Text(data,style: GoogleFonts.nunito(
+                color: Color(0xFFffb17a),
+                fontWeight: FontWeight.bold,
+                
+                fontSize: 25,
+                shadows: [
+                    const Shadow(
+                      blurRadius: 1.0,
+                      color: Colors.black,
+                      offset: Offset(0.0, 0.0),
+                    ),
+                  ]
+              ),),
+            ),),
+            const Positioned(child: seemore(),bottom: 0,)
+            ]
+        ),
+            
       ),
-          
     );
 
   }
--- lib/Widget/slidable.dart
+++ lib/Widget/slidable.dart
@@ -5,8 +5,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:iconly/iconly.dart';
 import 'package:slidable_button/slidable_button.dart';
 
-class Slidable extends StatelessWidget {
-  const Slidable({super.key});
+class seemore extends StatelessWidget {
+  
+  const seemore({super.key,});
 
   @override
   Widget build(BuildContext context) {
@@ -28,47 +29,47 @@ class Slidable extends StatelessWidget {
             child: Container(
               width: 180,
               height: 50,
-              color: Colors.black.withOpacity(0.1),
+              color: const Color(0xFFffb17a).withOpacity(0.2),
             ),
           ),
         ),
-        // Slidable button
+        
         Center(
-          child: Container(
-            width: 180,
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Stack(
-                children: [
-                  Center(child: Text("See more",style: GoogleFonts.nunito(
-                    color: Colors.white,
-                    fontSize: 15,
-                    shadows: [
-                        Shadow(
-                          blurRadius: 1.0,
-                          color: Colors.white,
-                          offset: const Offset(0.0, 0.0),
-                        ),
-                      ]
-                  ),),),
-                  Positioned(
-                    right: 0,
-                    
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 2.5),
-                      child: Container(
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(90),
-                          color: Colors.white,
-                        ),
-                        child: Icon(Icons.arrow_forward_ios_rounded)),
-                    )
-                  ),
-                ],
-              )
-            ),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 180,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Center(child: Text("See more",style: GoogleFonts.nunito(
+                        color: Color(0xFFffb17a),
+                        fontSize: 15,
+                        shadows: [
+                            
+                          ]
+                      ),),),
+                      Positioned(
+                        right: 0,
+                        
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 2.5),
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(90),
+                              color: Color(0xFF676f9d),
+                            ),
+                            child: Icon(Icons.arrow_forward_ios_rounded,color: Color(0xFFffb17a),)),
+                        )
+                      ),
+                    ],
+                  )
+                ),
+              ),
+            ],
           )
         ),
         ],
